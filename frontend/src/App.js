dd1726b8d228782eae89a30236264b703d813cfa
Ajas Bakran <ajas.bakran@external.t-systems.com>
2021-12-07 22:18:22 +0530

    Added react router and linkcontainers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Container } from "react-bootstrap";
-import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route exact path='/' element={<HomeScreen />} />
+            <Route exact path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
