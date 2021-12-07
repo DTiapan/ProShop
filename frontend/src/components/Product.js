25744317e8b6c28399e6fe2f027efe49fb1eb86b
Ajas Bakran <ajas.bakran@external.t-systems.com>
2021-12-07 21:31:31 +0530

    Added rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,5 @@
-import { Card } from "react-bootstrap";
-
+import { Card } from 'react-bootstrap';
+import Rating from '../components/Rating';
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
@@ -13,9 +13,11 @@ const Product = ({ product }) => {
           </Card.Title>
         </a>
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            color='#f8e825'
+          />
         </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
-import products from "../products";
-import Product from "../components/Product";
-import { Row, Col } from "react-bootstrap";
+import products from '../products';
+import Product from '../components/Product';
+import { Row, Col } from 'react-bootstrap';
 
 const HomeScreen = () => {
   return (
@@ -8,7 +8,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
